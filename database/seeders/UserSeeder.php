4852e73a564a96924eba32c8c4ece457deaf7b29
Arsalan Ahmad <arsalanwebdev@gmail.com>
2026-03-14 10:42:02 +0500

    roles created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::firstOrCreate(
+        [
+            'email' => 'admin@peakpeptides.com',
+        ],
+        [
+
+        'name' => 'Admin',
+        'password' => bcrypt('Admin@123')
+
+        ]
+    );
+    }
+}
